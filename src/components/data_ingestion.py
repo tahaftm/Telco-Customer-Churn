103cb7e64393a8d19a47310ec03e599f492cc1d4
Taha Tariq <tahatariqf@gmail.com>
2026-04-23 13:35:49 -0700

    Data ingestion done

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,38 @@
+import os
+import sys
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+from src.logger import logging
+import pandas as pd
+from src.exception import CustomException
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path:str = os.path.join("artifacts", "train.csv")
+    test_data_path:str = os.path.join("artifacts", "test.csv")
+    raw_data_path:str = os.path.join("artifacts", "raw.csv")
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+    
+    def initiate_data_ingestion(self):
+        try:
+            df = pd.read_csv("data\\CustomerChurn.csv")
+            logging.info("Loaded df")
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            train_df,test_df = train_test_split(df,test_size=0.2,random_state=42)
+            train_df.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_df.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            logging.info("Data splitted into training and testing sets")
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+if __name__ == "__main__":
+    data_ingestion = DataIngestion()
+    data_ingestion.initiate_data_ingestion()
